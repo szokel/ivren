055384a39354e2dd94f6151dd7fb1973da484394
Laszlo Szoke at pca <szoke.laszlo@gmail.com>
2026-04-19 16:10:55 +0200

    Initial structure: src layout, Core + WinForms

---
+++ src/Ivren.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Ivren.Core;
+
+public class Class1
+{
+
+}
---
+++ src/Ivren.WinForms/Form1.cs
@@ -0,0 +1,9 @@
+namespace Ivren.WinForms;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
